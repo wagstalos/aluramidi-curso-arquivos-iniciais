20abfa7194a41fccf65092d8043bc886b5b782b7
Wagner Paulo <wagstalos@hotmail.com>
2022-06-04 21:11:54 -0300

    first commit

---
+++ js/mains.js
@@ -0,0 +1,22 @@
+function playSound(idElementAudio){
+    document.querySelector(idElementAudio).play();
+}
+
+//document.querySelector('.tecla_pom').onclick = playPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function(){
+        playSound(idAudio)
+    };
+
+    contador ++;
+}
