94acad1e311074630096a90c8be5f8a8fbf66697
Wagner Paulo <wagstalos@hotmail.com>
2022-06-04 21:35:08 -0300

    add for

--- js/main.js
+++ js/main.js
@@ -5,18 +5,29 @@ function playSound(idElementAudio){
 //document.querySelector('.tecla_pom').onclick = playPom;
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+// let contador = 0;
+// while (contador < listaDeTeclas.length){
 
-while (contador < listaDeTeclas.length){
+//     const tecla = listaDeTeclas[contador];
+//     const instrumento = tecla.classList[1];
+
+//     const idAudio = `#som_${instrumento}`;
+
+//     tecla.onclick = function(){
+//         playSound(idAudio)
+//     };
+
+//     contador ++;
+// }
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-
     const idAudio = `#som_${instrumento}`;
 
     tecla.onclick = function(){
         playSound(idAudio)
     };
 
-    contador ++;
 }
